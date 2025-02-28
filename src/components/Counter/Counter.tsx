21df9455a190bf618cf8ac4e419b440374427d9d
zeeshan447 <41149139+zeeshan447@users.noreply.github.com>
2025-02-28 17:12:07 +0100

    code clean up

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,23 +1,17 @@
-import { Box, Flex, Text, useToast } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, useToast } from "@chakra-ui/react";
 import { useCounter } from "../../hooks/useCounter";
+
 type Props = {};
 
 const Counter = (props: Props) => {
   const { count, increment } = useCounter();
-  const toast = useToast({
-    position: "top",
-    title: "Container style is updated",
-    containerStyle: {
-      width: "800px",
-      maxWidth: "100%",
-    },
-  });
+  const toast = useToast();
 
   return (
     <Box pos="relative">
       <Flex justify="center" align="center" w="100%" h="100vh" gap={4}>
         <Text>Current count {count} </Text>
-        <Box
+        <Button
           onClick={() => {
             increment();
             toast({
@@ -26,55 +20,7 @@ const Counter = (props: Props) => {
               containerStyle: {
                 width: "300px",
               },
-
-              render: () => (
-                <Box
-                  style={{
-                    position: "relative",
-                    borderRadius: "8px",
-                    padding: "1px",
-                    background:
-                      "linear-gradient(white, white) padding-box, radial-gradient(53.57% 282.15% at 2.14% 50%, rgba(116, 200, 152, 0.65) 0%, rgba(116, 200, 152, 0.1) 100%) border-box",
-                    border: "2px solid transparent",
-                    backgroundClip: "padding-box, border-box",
-                  }}
-                >
-                  <Box
-                    position="relative"
-                    zIndex={1}
-                    color="white"
-                    p={3}
-                    bg="red"
-                    borderRadius="6px"
-                    backgroundImage="radial-gradient(53.57% 282.15% at 2.14% 50%, rgba(116,200, 152, 0.15) 0%, rgba(116, 200, 152, 0.03) 100%), #46474F"
-                    background="#46474F"
-                    boxShadow="0px 0px 0px 1px rgba(40, 41, 50, 0.04), 0px 2px 2px -1px rgba(40, 41, 50, 0.04), 0px 4px 4px -2px rgba(40, 41, 50, 0.04), 0px 8px 8px -4px rgba(40, 41, 50, 0.06), 0px 16px 32px rgba(40, 41, 50,0.06)"
-                  >
-                    <Flex gap={2} align="center">
-                      <img src="/check-icon.svg" alt="check icon" />
-                      <Flex direction="column">
-                        <Text
-                          fontWeight={500}
-                          fontSize={14}
-                          lineHeight="16px"
-                          color="#ffffff"
-                        >
-                          Incremented
-                        </Text>
-                        <Text
-                          fontWeight={500}
-                          fontSize={14}
-                          lineHeight="16px"
-                          color="#ffffff"
-                        >
-                          {/*state updates in React are asynchronous  */}
-                          Counter is now {count + 1}
-                        </Text>
-                      </Flex>
-                    </Flex>
-                  </Box>
-                </Box>
-              ),
+              render: () => <TostContainer count={count} />,
             });
           }}
           sx={{
@@ -86,10 +32,65 @@ const Counter = (props: Props) => {
           }}
         >
           +1
-        </Box>
+        </Button>
       </Flex>
     </Box>
   );
 };
 
 export default Counter;
+
+type TostContainerProps = {
+  count: number;
+};
+
+const TostContainer = ({ count }: TostContainerProps) => {
+  return (
+    <Box
+      style={{
+        position: "relative",
+        borderRadius: "8px",
+        padding: "1px",
+        background:
+          "linear-gradient(white, white) padding-box, radial-gradient(53.57% 282.15% at 2.14% 50%, rgba(116, 200, 152, 0.65) 0%, rgba(116, 200, 152, 0.1) 100%) border-box",
+        border: "2px solid transparent",
+        backgroundClip: "padding-box, border-box",
+      }}
+    >
+      <Box
+        position="relative"
+        zIndex={1}
+        color="white"
+        p={3}
+        bg="red"
+        borderRadius="6px"
+        backgroundImage="radial-gradient(53.57% 282.15% at 2.14% 50%, rgba(116,200, 152, 0.15) 0%, rgba(116, 200, 152, 0.03) 100%), #46474F"
+        background="#46474F"
+        boxShadow="0px 0px 0px 1px rgba(40, 41, 50, 0.04), 0px 2px 2px -1px rgba(40, 41, 50, 0.04), 0px 4px 4px -2px rgba(40, 41, 50, 0.04), 0px 8px 8px -4px rgba(40, 41, 50, 0.06), 0px 16px 32px rgba(40, 41, 50,0.06)"
+      >
+        <Flex gap={2} align="center">
+          <img src="/check-icon.svg" alt="check icon" />
+          <Flex direction="column">
+            <Text
+              fontWeight={500}
+              fontSize={14}
+              lineHeight="16px"
+              color="#ffffff"
+            >
+              Incremented
+            </Text>
+            <Text
+              fontWeight={500}
+              fontSize={14}
+              lineHeight="16px"
+              color="#ffffff"
+            >
+              {/* State updates in React are asynchronous hence the +1. There are other workarounds as well. */}
+              Counter is now {count + 1}
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
